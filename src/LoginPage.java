cfccb2c1fa5670c58b26b469b271fce194299cad
HomePC <tarikcollu@gmail.com>
2023-05-20 01:23:30 -0400

    I added new functionality to login

---
+++ src/LoginPage.java
@@ -0,0 +1,12 @@
+public class LoginPage {
+
+    String userName;
+    String password;
+
+    public LoginPage(String userName, String password) {
+
+        this.userName = userName;
+        this.password = password;
+
+    }
+}
